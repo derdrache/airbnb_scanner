b4f1b8e7837351f3250f6c2e657852fd31b3cafa
derdrache <dominik.mast.11@gmail.com>
2021-06-17 22:35:45 +0200

    test

--- gui/ergebnisse_tabelle_fenster.py
+++ gui/ergebnisse_tabelle_fenster.py
@@ -3,21 +3,18 @@ from tkinter import messagebox
 from tkinter import ttk
 from ttkthemes import ThemedStyle
 import html
+import webbrowser
 
 
 def ergebnissPage(df):
-    global sortierungsPrioList
-
-    sortierungsPrioList = []
+    global resultTable
 
     tableWindow = tk.Toplevel()
     tableWindow.title("Airbnb Ergebnisse")
     tableWindow.bind("<Motion>", callback)
-
     setStyle(tableWindow)
 
-    table = Table(tableWindow, df)
-
+    resultTable = ResultTable(tableWindow, df)
     createTableExtraData(tableWindow, df)
 
 def callback(event):
@@ -32,37 +29,31 @@ def setStyle(window):
                     background="lightgrey",
                     fieldbackground="lightgrey")
 
-def expandFullWindowSize(window):
-    window.grid_rowconfigure(0, weight=1)
-    window.grid_columnconfigure(0, weight=1)
-
-class Table:
+class ResultTable:
     def __init__(self, root, df):
 
         self.sortierungsPrioList = []
 
-        columnsList = list(df.columns)
-        self.table = ttk.Treeview(root, column=tuple(columnsList))
+        columnList = list(df.columns)
+        self.table = ttk.Treeview(root, column=tuple(columnList))
         self.table["show"] = "headings"
-        self.table.bind("<Double-1>", self.openTableLink)
+        self.table.bind("<Double-1>", self.openRoomlinkInBrowser)
 
-        self.fillTableHead(columnsList)
+        self.fillTableHead(columnList)
         self.fillTableBody(df)
 
         self.table.pack(fill = "both" ,expand=True)
 
-    def openTableLink(self,selection):
-        selectionID = self.table.selection()
-        inputItem = self.table.item(selectionID)['values'][9]#Links
+    def openRoomlinkInBrowser(self,event):
+        selection = self.table.selection()
+        urlLink = self.table.item(selection)['values'][9]
 
-        import webbrowser
-        webbrowser.open('{}'.format(inputItem))
+        webbrowser.open('{}'.format(urlLink))
 
-
-    def fillTableHead(self,cols):
-        for index, col in enumerate(cols):
+    def fillTableHead(self,columns):
+        for index, col in enumerate(columns):
             columnWidth = self.getTableColumnWidt(col)
-            self.table.heading(col, text=col, anchor='w', command=lambda _col=col: self.popupWindowTable(_col))
+            self.table.heading(col, text=col, anchor='w', command=lambda _col=col: PopupWindow(_col))
             self.table.column(col, anchor="w", width=columnWidth)
 
     def getTableColumnWidt(self,col):
@@ -79,13 +70,18 @@ class Table:
         else:
             return 50
 
-    def popupWindowTable(self,col):
-        global mousePosX, mousePosY
+    def fillTableBody(self,df):
+        for index, row in df.iterrows():
+            self.table.insert("",tk.END,text="", value =list(row))
 
+class PopupWindow:
+    def __init__(self, col):
         self.popupWindow  = tk.Tk()
         self.popupWindow.title(col)
 
-        setStyle(self.popupWindow)
+        self.setStyle()
+
+        self.sortierungsPrioList = []
 
         popupFrame = ttk.Frame(self.popupWindow)
 
@@ -112,7 +108,11 @@ class Table:
 
         popupFrame.pack()
 
-    def buttonTableSortColumn(self,col, reverse):
+    def setStyle(self):
+        style = ThemedStyle(self.popupWindow)
+        style.set_theme("clearlooks")
+
+    def buttonTableSortColumn(self, col, reverse):
 
         self.sortPrio(col, reverse)
         self.tableHeaderChange()
@@ -120,7 +120,7 @@ class Table:
 
         self.popupWindow.destroy()
 
-    def sortPrio(self,col, reverse):
+    def sortPrio(self, col, reverse):
         if len(self.sortierungsPrioList) == 0:
             self.sortierungsPrioList.append([col , reverse, ""])
         else:
@@ -133,7 +133,7 @@ class Table:
     def tableHeaderChange(self):
         for index, item in enumerate(self.sortierungsPrioList):
             headerChange = item[0] + " " + str(index +1) + " " + self.getArrowForSort(item[1])
-            self.table.heading(item[0], text=headerChange)
+            resultTable.table.heading(item[0], text=headerChange)
 
     def getArrowForSort(self,reverse):
         pfeilHoch = html.unescape("&#x2191;")
@@ -145,14 +145,14 @@ class Table:
             return pfeilRunter
 
     def sortColumn(self):
-        rowIDArr = self.table.get_children('')
+        rowIDArr = resultTable.table.get_children('')
         colDataArr = []
 
         #Daten von jeder Reihe mit den jeweiligen Spalten mit der ID speichern
         for rowID in rowIDArr:
             data = []
             for col in self.sortierungsPrioList:
-                tableData = self.table.set(rowID, col[0]) #aus der Tabelle
+                tableData = resultTable.table.set(rowID, col[0]) #aus der Tabelle
                 if tableData.isdigit():
                     tableData = int("".join([i for i in tableData if i.isdigit()]))#prüfen ob Zahlen
                 if col[1] == True:
@@ -166,7 +166,7 @@ class Table:
 
         # re arrange items in sorted positions
         for index, value in enumerate(colDataArr):
-            self.table.move(value[-1], '', index)
+            resultTable.table.move(value[-1], '', index)
 
     class reversor:
         def __init__(self, obj):
@@ -184,74 +184,13 @@ class Table:
     def buttonTableResetColumn(self,col):
         for item in self.sortierungsPrioList:
             if item[0] == col:
-                self.table.heading(col, text=col)
+                resultTable.table.heading(col, text=col)
                 self.sortierungsPrioList.remove(item)
 
         self.tableHeaderChange()
         self.sortColumn()
 
         self.popupWindow.destroy()
-
-
-    def fillTableBody(self,df):
-        for index, row in df.iterrows():
-            self.table.insert("",tk.END,text="", value =list(row))
-
-class PopupWindow:
-    def __init__(self, table, col):
-        self.popupWindow  = tk.Tk()
-        self.popupWindow.title(col)
-
-        self.setStyle(popupWindow)
-
-
-        popupFrame = ttk.Frame(popupWindow)
-
-        buttonSortierungAZ = ttk.Button(popupFrame,
-                                text= "Sort A-Z",
-                                command= lambda: self.buttonTableSortColumn(col, False))
-        buttonSortierungAZ.pack()
-
-        buttonSortierungZA = ttk.Button(popupFrame,
-                                text= "Sort Z-A",
-                                command= lambda: self.buttonTableSortColumn(col, True))
-        buttonSortierungZA.pack()
-
-        buttonFilter = ttk.Button(popupFrame,
-                                text= "Filter",
-                                command= lambda: tabelFilterColumn(col))
-        buttonFilter.pack()
-
-        buttonReset = ttk.Button(popupFrame,
-                                text= "reset",
-                                command= lambda: buttonTableResetColumn(col))
-        buttonReset.pack()
-
-
-        popupFrame.pack()
-
-    def setStyle(self,window):
-        style = ThemedStyle(window)
-        style.set_theme("clearlooks")
-
-    def buttonTableSortColumn(self, col, reverse):
-
-        sortPrio(col, reverse)
-        tableHeaderChange()
-        sortColumn()
-
-        popupWindow.destroy()
-
-    def sortPrio(self, col, reverse):
-        if len(sortierungsPrioList) == 0:
-            sortierungsPrioList.append([col , reverse, ""])
-        else:
-            for index, item in enumerate(sortierungsPrioList):
-                if item[0] == col:
-                    sortierungsPrioList[index][1] = reverse
-                else:
-                    sortierungsPrioList.append([col , reverse, ""])
-
 
 def createTableExtraData(window, df):
     tableWindowFrameB = ttk.Frame(window)
